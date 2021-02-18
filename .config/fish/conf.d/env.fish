97dda4f2fbdf31ddedfd78f78579b0b844f8b3ac
AokabiC <syht0312@gmail.com>
2021-02-18 22:03:10 +0900

    comment out on defauly

--- .config/fish/conf.d/env.fish
+++ .config/fish/conf.d/env.fish
@@ -1,13 +1,13 @@
 set -U fish_user_paths $fish_user_paths /usr/local/bin
 
-# cargo
-source ~/.cargo/env
+# # cargo
+# source ~/.cargo/env
 
-# volta
-set -x VOLTA_HOME $HOME/.volta
-set -x PATH $VOLTA_HOME/bin $PATH
+# # volta
+# set -x VOLTA_HOME $HOME/.volta
+# set -x PATH $VOLTA_HOME/bin $PATH
 
-# pyenv
-set -x PYENV_ROOT $HOME/.pyenv
-set -x PATH $PYENV_ROOT/bin $PATH
-pyenv init - | source
+# # pyenv
+# set -x PYENV_ROOT $HOME/.pyenv
+# set -x PATH $PYENV_ROOT/bin $PATH
+# pyenv init - | source
